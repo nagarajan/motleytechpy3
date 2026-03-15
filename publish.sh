9ad9c452d251181763d6f5f81918c5cec94bd21c
Rajan Nagarajan <rnagarajan@zoox.com>
2026-03-15 01:10:20 -0700

    delete tasks folder before copying

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ git pull
 echo "Pulled latest changes"
 cp -r output/* /var/www/motleytech/html
 cd tasks
+rm -rf /var/www/motleytech/html/tasks
 cp -r dist /var/www/motleytech/html/tasks
 echo "Copied dist to /var/www/motleytech/html/tasks"
 sed -i 's|/assets|assets|g' /var/www/motleytech/html/tasks/index.html
